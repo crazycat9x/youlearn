aa1736b223d262dea9fb7e9c446579867b5f8f3c
hdg-utd <hdg150130@utdallas.edu>
2017-09-16 02:36:14 -0400

    Search button works

--- myscript.js
+++ myscript.js
@@ -1,15 +1,18 @@
-$("#search-form").append("<button id=\"learn-search\">Learn</button>");
+$("#search-form").append("<a id=\"learn-search\">You Learn</a>");
 $("#learn-search").css({
   "margin-left": "20px",
-  "width": "100px",
+  "padding": "10px",
   "font-family": "Roboto, Arial, sans-serif",
-  "font-size": "16px" 
+  "font-size": "16px",
+  "border": "1px solid gray",
+  "border-radius": "10px",
+  "background-color": "lightgray"
 });
 
 $("#learn-search").click(function() {
-  chrome.runtime.sendMessage({specialClick: "yes"}, function(response) {});
-});
-
-$("#search-icon-legacy").click(function() {
-  chrome.runtime.sendMessage({specialClick: "no"}, function(response) {});
+  chrome.runtime.sendMessage({
+    clicked: "yes", 
+    searchTerm: $("#search-input #search").val()
+  }, function(response) {
+  });
 });
