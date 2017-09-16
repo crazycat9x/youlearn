fac03e8622ae16dd4d1046af7a31e73d46d1726b
hdg-utd <hdg150130@utdallas.edu>
2017-09-16 01:21:36 -0400

    Added special search button

--- myscript.js
+++ myscript.js
@@ -1,1 +1,7 @@
-console.log("myscript");
+$("#search-form").append("<button id=\"learn-search\">Learn</button>");
+$("#learn-search").css({
+  'margin-left': '20px',
+  'width': '100px',
+  'font-family': 'Roboto, Arial, sans-serif',
+  'font-size': '16px' 
+});
