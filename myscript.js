a3bf43dd4926a98b172c8cc04bc5ca4062c13169
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2017-09-16 23:41:30 -0400

    add image to button

--- myscript.js
+++ myscript.js
@@ -1,10 +1,16 @@
-$("#search-form").append("<a class=\"btn red\" id=\"learn-search\">You Learn</a>");
+var img = document.createElement("IMG");
+img.src = chrome.runtime.getURL('searchWithYouLearnLogo.png');
+img.style.setProperty('width','70px');
+$("#search-form").append("<a class=\"btn red\" id=\"learn-search\">\
+</a>");
+$("#learn-search").append(img)
 $("#learn-search").css({
   "margin-left": "20px",
   "padding": "10px",
   "font-family": "Roboto, Arial, sans-serif",
   "font-size": "16px",
 });
+
 
 $("#learn-search").click(function() {
   chrome.runtime.sendMessage({
