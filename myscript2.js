2d15fff4a64286a9f42695a84ad52d94aba9cef6
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2017-09-16 20:41:10 -0400

    Add dropdown

--- myscript2.js
+++ myscript2.js
@@ -18,7 +18,6 @@ var rating = "\
     <i class='fa fa-star fa-fw'></i>\
   </li>\
 </ul>\
-<<<<<<< HEAD
 </div>\
 <select>\
 <option value=\"useful\">Useful</option>\
@@ -30,36 +29,6 @@ $("div#contents.style-scope.ytd-item-section-renderer > ytd-playlist-renderer" )
 })
 console.log(rating)
 
-=======
-</div>"
-
-var ratingScore = "\
-<div class='rating-stars text-center'>\
-<ul id='overallStars'>\
-  <li class='star overall' title='Poor' data-value='1'>\
-    <i class='fa fa-star fa-fw'></i>\
-  </li>\
-  <li class='star overall' title='Fair' data-value='2'>\
-    <i class='fa fa-star fa-fw'></i>\
-  </li>\
-  <li class='star overall' title='Good' data-value='3'>\
-    <i class='fa fa-star fa-fw'></i>\
-  </li>\
-  <li class='star overall' title='Excellent' data-value='4'>\
-    <i class='fa fa-star fa-fw'></i>\
-  </li>\
-  <li class='star overall' title='WOW!!!' data-value='5'>\
-    <i class='fa fa-star fa-fw'></i>\
-  </li>\
-  <li class='scoreNum'>3.5</li>\
-</ul>\
-</div>"
-$(ratingScore).appendTo("ytd-video-meta-block#meta.style-scope.ytd-playlist-renderer")
-$(rating).appendTo("div#content.style-scope.ytd-playlist-renderer")
-
-//console.log(rating)
-$(document).ready(function(){
->>>>>>> 87a79532d1bde94151bc8ea24e8438da43766b66
     
     $("#content a.ytd-playlist-renderer").each(function(index) {
       var link = $(this).attr('href');   
